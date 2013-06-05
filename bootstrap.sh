133cdea5a5ddc94990318040ccb3c51e1352bf54
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-05 00:37:20 -0700

    An archetype can be passed as a command line argument.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-options=("archetypes/clojure" "archetypes/java")
-select archetype in "${options[@]}"; do
+ARCHETYPE="$1"
+if [ -z "$ARCHETYPE" ]; then
+  options=("archetypes/clojure" "archetypes/java")
+  select archetype in "${options[@]}"; do
+    break
+  done
+fi
 
-  read -p "PROJECT NAME: " PROJECT
-  git clone --origin archetype "https://github.com/$archetype.git" "$PROJECT"
+echo ""
+echo "## Creating a new $ARCHETYPE project"
+read -p "PROJECT NAME: " PROJECT
+git clone --origin archetype "https://github.com/$ARCHETYPE.git" "$PROJECT" || exit $?
 
-  echo "Checked out into $PROJECT"
-  echo "Next steps:"
-  echo "  cd \"$PROJECT\""
-  echo "  edit properties.yml"
-  echo "  ./init.rb"
-  echo "  git add -A"
-  echo "  git commit -m \"Initialize project.\""
-  echo "  git clean -df"
-  break
-done
+echo "Checked out into $PROJECT"
+echo "Next steps:"
+echo "  cd \"$PROJECT\""
+echo "  edit properties.yml"
+echo "  ./init.rb"
+echo "  git add -A"
+echo "  git commit -m \"Initialize project.\""
+echo "  git clean -df"
